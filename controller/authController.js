18cbbb70dde04ce982402588aeb119868b7ce859
asally <a.salawu@outlook.com>
2023-06-12 12:56:53 +0000

    all routes working

--- controller/authController.js
+++ controller/authController.js
@@ -1,15 +1,17 @@
 
 const signup_get = (req, res) =>{
-    res.send('login page')
+    res.send('signup page')
 }
 const login_get = (req, res) =>{
     res.send('login page')
 }
 const signup_post = (req, res) =>{
-    res.send('new signup')
+    const {email, username} = req.body
+    res.send(`${username} with email:${email} has signed up`)
 }
 const login_post = (req, res) =>{
-    res.send('user logged in')
+    const {email, username} = req.body
+    res.send(`${username} with email:${email} has logged in`)
 }
 
 module.exports = { signup_get, signup_post, login_get, login_post}
