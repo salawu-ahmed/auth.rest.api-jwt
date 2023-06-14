140cab64c64e7ecc573ac477f0edf2b277feed7f
asally <a.salawu@outlook.com>
2023-06-14 14:41:50 +0000

    creating & reading cookies with cookie-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config()
 
@@ -18,7 +19,24 @@ const authRoutes = require('./routes/authRoutes')
 // MIDDLE WARES
 app.use(cors())
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use('/', authRoutes)
+
+app.get('/setCookies', (req, res) => {
+    res.setHeader('Set-Cookie', 'name = true')
+    res.cookie('lastname', 'shawn')
+    // maxAge is in milliseconds
+    // httpOnly it cannot be accessed on client side 
+    // secure sends the cookie only over https
+    // use https connection for production purposes 
+    res.cookie('isEmployed', true, { maxAge: 1000 * 60 * 60 * 24, secure: true, httpOnly: true })
+    res.send('you got the cookies')
+})
+
+app.get('/readCookies', (req, res) => {
+    const cookies = req.cookies
+    res.json(cookies)
+})
 
 
 PORT = process.env.CONNECTION_PORT || 4000
